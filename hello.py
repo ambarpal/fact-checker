ebdd605b758ce32aaa50985e86e35d90e66781ca
Ambar Pal <ambar@io6.cis.jhu.edu>
2020-09-12 16:53:54 -0400

    heading towards merge conflict

--- hello.py
+++ hello.py
@@ -3,10 +3,21 @@ import pdb
 import torch
 import sys
 
-# sys.path.insert(0, './cis/home/ambar/my_documents/docker-data/com/hophacks20')
+import os
+print (os.getcwd())
+print (os.listdir())
+
+sys.path.insert(0, './cis/home/ambar/my_documents/docker-data/com/hophacks20')
+
+print (os.listdir())
 from test_search_engine import check_true
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    print ('i am called')
+    return render_template('index.html')
 
 # @app.route('/')
 # def index():
@@ -27,11 +38,11 @@ def post2():
     print('here')
     text = request.form['query']
     response = check_true(text)
-    response = text.upper()
     return render_template('result.html', result=response)
 
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+    
 
-@app.route('/', methods=['GET'])
-def index():
-    print ('i am called')
-    return render_template('index.html')
+
+
