b707771b95b54e4a4ceadb74f31691b9af328e09
Ishita Tripathi <sakurakhadag@gmail.com>
2020-09-12 16:00:42 -0400

    Fixed post issue

--- hello.py
+++ hello.py
@@ -8,22 +8,34 @@ app = Flask(__name__)
 #     posts = [{'title': 'Some Title', 'created': '21 jan'}]
 #     return render_template('index.html', posts=posts)
 
-@app.route('/', methods=['GET'])
-def index():
-    print ('i am called')
-    return render_template('index.html')
 
+'''
 @app.route('/<string:query>')
 def post(query):
     res = query + " Hello"
     # pdb.set_trace()
     return render_template('result.html', result=res)
+'''
 
-@app.route('/r', methods=['POST'])
+@app.route('/', methods=['POST'])
 def post2():
+    print('here')
     text = request.form['query']
     print ('dsfsdfsdf')
     print (text)
     print ('dsfsdfsdf')
     processed_text = text.upper()
     return render_template('result.html', result=processed_text)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    print ('i am called')
+    return render_template('index.html')
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    projectpath = request.form['projectFilepath']
+    # your code
+    # return a response
+    return render_template('result.html', result=projectpath)
