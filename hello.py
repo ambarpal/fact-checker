d124d1188235d53e219d22ebd4b12179aa0a5e15
Ishita Tripathi <sakurakhadag@gmail.com>
2020-09-13 02:28:07 -0400

    Format page 2, add link back to page 1

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ import os
 
 #ishita's laptop does not support BERT
 if os.getcwd() != 'C:\\Users\\sakur\\Desktop\\Hophacks\\website\\fact-checker':
+    print(os.getcdw())
     sys.path.insert(0, './cis/home/ambar/my_documents/docker-data/com/hophacks20')
     from test_search_engine import check_true
 
@@ -26,16 +27,22 @@ def post2():
     print('here')
     text = request.form['query']
     try:
-        response = check_true(text)
+        #response = check_true(text)
+        response = 0
         response = ['False', 'Unsure', 'True'][response * 2]
     except:
         response = 'Unsure'
-        
-    return render_template('result.html', result=response)
+
+    return render_template('results2.html', result=response)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
-    
 
 
-
+'''
+<!--
+{{@for_{variable in variable}}
+<div class='source'> {{ variable['text'] }} </div>
+{{end_for}}}
+-->
+'''
